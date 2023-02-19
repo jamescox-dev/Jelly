d63ba9acebe5de9c9d63e97434dde0d82584037d
James Cox <james@jamescox.info>
2023-02-19 12:32:16 +0000

    Added NumberValue.

---
+++ Jelly.Tests/Values/NumberValueTests.cs
@@ -0,0 +1,163 @@
+namespace Jelly.Values.Tests;
+
+using Jelly.Values;
+
+[TestFixture]
+public class NumberValueTests
+{
+    [TestCase(0.0)]
+    [TestCase(0.84551240822557006)]
+    [TestCase(Math.PI)]
+    public void ANumberValuesStringRepresentationCanBeUsedToConstructAnIdenticalNumberValue(double number)
+    {
+        var num = new NumberValue(number);
+
+        var str = num.ToString();
+
+        double.Parse(str).Should().Be(number);
+    }
+
+    [TestCase("this is not a number")]
+    [TestCase("_0")]
+    [TestCase("0_")]
+    [TestCase("_0_")]
+    [TestCase("._0")]
+    [TestCase(".0_")]
+    [TestCase("._0_")]
+    [TestCase(".0e_1")]
+    [TestCase(".0e1_")]
+    [TestCase(".0e_1_")]
+    [TestCase("0x_1")]
+    [TestCase("0x1_")]
+    [TestCase("0x_1_")]
+    [TestCase("0o_1")]
+    [TestCase("0o1_")]
+    [TestCase("0o_1_")]
+    [TestCase("0b_1")]
+    [TestCase("0b1_")]
+    [TestCase("0b_1_")]
+    [TestCase(".")]
+    [TestCase("e")]
+    public void ParsingANumberResultsInANaNWhenTheStringIsNotInAValidFormat(string numberString)
+    {
+        var number = NumberValue.ParseNumber(numberString);
+
+        double.IsNaN(number).Should().BeTrue();
+    }
+
+    [TestCase("true", 1.0)]
+    [TestCase("True", 1.0)]
+    [TestCase("TRUE", 1.0)]
+    [TestCase("false", 0.0)]
+    [TestCase("FALSE", 0.0)]
+    [TestCase("fAlSe", 0.0)]
+    [TestCase("inf", double.PositiveInfinity)]
+    [TestCase("INF", double.PositiveInfinity)]
+    [TestCase("inF", double.PositiveInfinity)]
+    [TestCase("-inf", double.NegativeInfinity)]
+    [TestCase("-Inf", double.NegativeInfinity)]
+    [TestCase("-InF", double.NegativeInfinity)]
+    public void SpecialWordsAreParsedIntoThereCorrectValue(string numberString, double expected)
+    {
+        var number = NumberValue.ParseNumber(numberString);
+
+        number.Should().Be(expected);
+    }
+
+    [TestCase("  true", 1.0)]
+    [TestCase("  inf  \n\t", double.PositiveInfinity)]
+    [TestCase("false\t\t", 0.0)]
+    public void LeadingAndTrailingSpacesAreIgnoredWhenParsingANumber(string numberString, double expected)
+    {
+        var number = NumberValue.ParseNumber(numberString);
+
+        number.Should().Be(expected);
+    }
+    
+    [TestCase("0", 0.0)]
+    [TestCase("42", 42.0)]
+    [TestCase("-42", -42.0)]
+    [TestCase("+10", 10.0)]
+    [TestCase("810038390800", 810038390800.0)]
+    [TestCase("100000000000000000000", 100000000000000000000.0)]
+    [TestCase("-99999999999999999999999", -99999999999999999999999.0)]
+    public void IntegerCanBeParsed(string numberString, double expected)
+    {
+        var number = NumberValue.ParseNumber(numberString);
+
+        number.Should().Be(expected);
+    }
+
+    [TestCase("0.05", 0.05)]
+    [TestCase("+1.876e10", 1.876e10)]
+    [TestCase("-677e28", -677e28)]
+    [TestCase("-0.05", -0.05)]
+    [TestCase("1.98676876876876", 1.98676876876876)]
+    public void DecimalNumbersCanBeParsed(string numberString, double expected)
+    {
+        var number = NumberValue.ParseNumber(numberString);
+
+        number.Should().Be(expected);
+    }
+
+    [TestCase("0xff", 255)]
+    [TestCase("0x100", 256)]
+    [TestCase("0x100000000000", 0x100000000000)]
+    [TestCase("-0xf", -15)]
+    public void HexadecimalNumbersCanBeParsed(string numberString, double expected)
+    {
+        var number = NumberValue.ParseNumber(numberString);
+
+        number.Should().Be(expected);
+    }
+
+    [TestCase("0xfffffffffffff800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001", 1)]
+    [TestCase("-0xfffffffffffff800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001", -1)]
+    [TestCase("1.7976931348623159e308", 1)]
+    [TestCase("-1.7976931348623159e308", -1)]
+    public void WhenTheNumberIsOutOfTheRangeOfADoubleFloatAInfinityIsReturned(string numberString, int expectedSign)
+    {
+        var number = NumberValue.ParseNumber(numberString);
+
+        Math.Sign(number).Should().Be(expectedSign);
+        double.IsInfinity(number).Should().BeTrue();
+    }
+
+    [TestCase("1_000_000", 1_000_000.0)]
+    [TestCase("3.141_593", 3.141_593)]
+    [TestCase("1_234.567_890", 1_234.567_890)]
+    [TestCase("0.123_456", 0.123_456)]
+    [TestCase("1_000_000e1_0", 1_000_000e1_0)]
+    [TestCase("3.141_593e1_1", 3.141_593e1_1)]
+    [TestCase("1_234.567_890e0_1", 1_234.567_890e0_1)]
+    [TestCase("0.123_456e4_2", 0.123_456e4_2)]
+    [TestCase("0xffff_ffff", 0xffff_ffff)]
+    public void UnderscoresAreIgnoredBetweenGroupsOfDigits(string numberString, double expected)
+    {
+        var number = NumberValue.ParseNumber(numberString);
+
+        number.Should().Be(expected);
+    }
+
+    [TestCase("0b11", 3)]
+    [TestCase("0b100", 4)]
+    [TestCase("0b1000_0000_0000", 0b100000000000)]
+    [TestCase("-0b1", -1)]
+    public void BinaryNumbersCanBeParsed(string numberString, double expected)
+    {
+        var number = NumberValue.ParseNumber(numberString);
+
+        number.Should().Be(expected);
+    }
+
+    [TestCase("0o77", 63)]
+    [TestCase("0o100", 64)]
+    [TestCase("0o300_200_100", 50397248)]
+    [TestCase("-0o7", -7)]
+    public void OctalNumbersCanBeParsed(string numberString, double expected)
+    {
+        var number = NumberValue.ParseNumber(numberString);
+
+        number.Should().Be(expected);
+    }
+}
---
+++ Jelly/Values/NumberValue.cs
@@ -0,0 +1,222 @@
+namespace Jelly.Values;
+
+using System.Globalization;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+public class NumberValue : Value
+{
+    static readonly BigInteger DoubleMaxValueAsBigInt = new BigInteger(double.MaxValue);
+    static readonly Regex NumberPattern = new Regex(@"
+        ^\s*
+        [+-]?
+        (
+            (
+                ([0-9]|[0-9][0-9_]*?[0-9])
+                (
+                    \.
+                |
+                    \.([0-9]|[0-9][0-9_]*?[0-9])
+                )?
+            )
+            (e[+-]?([0-9]|[0-9][0-9_]*?[0-9]))?
+        |
+            (0x([0-9a-f]|[0-9a-f][0-9a-f_]*?[0-9a-f]))
+        |
+            (0o([0-7]|[0-7][0-7_]*?[0-7]))
+        |
+            (0b([01]|[01][01_]*?[01]))
+        |
+            (inf|nan|true|false)
+        )
+        \s*$
+    ", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace | RegexOptions.CultureInvariant);
+
+    static readonly Dictionary<string, double> SpecialWords = new(StringComparer.InvariantCultureIgnoreCase)
+    {
+        { "true", 1.0 }, { "false", 0.0 }, { "inf", double.PositiveInfinity }, { "-inf", double.NegativeInfinity }
+    };
+
+    readonly double _value;
+
+    public NumberValue(double value)
+    {
+        _value = value;
+    }
+
+    public NumberValue(string value)
+    {
+        _value = double.Parse(value, NumberStyles.AllowLeadingWhite, CultureInfo.InvariantCulture);
+    }
+
+    public override double ToDouble() => _value;
+
+    public override string ToString()
+    {
+        return _value.ToString("G17");
+    }
+
+    public static double ParseNumber(string number)
+    {
+        if (ValidateAndFixNumber(ref number))
+        {
+            if (TryParseDecimal(number, out var parsedDecimal))
+            {
+                return parsedDecimal;
+            }
+            if (TryParseHexademimal(number, out var parsedHexadecimal))
+            {
+                return parsedHexadecimal;
+            }
+            if (TryParseSpecialWord(number, out var parsedWordValue))
+            {
+                return parsedWordValue;
+            }
+            if (TryParseBinary(number, out var parsedBinary))
+            {
+                return parsedBinary;
+            }
+            if (TryParseOctal(number, out var parsedOctal))
+            {
+                return parsedOctal;
+            }
+        }
+        return double.NaN;
+    }
+
+    public static bool IsValidNumber(string number)
+    {
+        return NumberPattern.IsMatch(number);
+    }
+
+    static bool ValidateAndFixNumber(ref string number)
+    {
+        if (IsValidNumber(number))
+        {
+            number = number.Trim().Replace("_", "");
+            return true;
+        }
+        return false;
+    }
+
+    static bool TryParseSpecialWord(string number, out double value)
+    {
+        return SpecialWords.TryGetValue(number, out value);
+    }
+
+    static bool TryParseDecimal(string number, out double value)
+    {
+        return double.TryParse(number, NumberStyles.AllowLeadingSign | NumberStyles.AllowDecimalPoint | NumberStyles.AllowExponent, CultureInfo.InvariantCulture, out value);
+    }
+
+    static bool TryParseHexademimal(string number, out double value)
+    {
+        var sign = 1;
+        if (number.StartsWith('-'))
+        {
+            number = number.Substring(1);
+            sign = -1;
+        }
+        if (number.StartsWith("0x", StringComparison.CurrentCultureIgnoreCase))
+        {
+            number = $"0{number.Substring(2)}";
+            if (BigInteger.TryParse(number, NumberStyles.AllowHexSpecifier, CultureInfo.InvariantCulture, out var parsedBigInt))
+            {
+                if (parsedBigInt > DoubleMaxValueAsBigInt)
+                {
+                    value = double.PositiveInfinity * sign;     
+                }
+                else
+                {
+                    value = ((double)parsedBigInt) * sign;
+                }
+                return true;
+            }
+        }
+
+        value = 0.0;
+        return false;
+    }
+
+    static bool TryParseBinary(string number, out double value)
+    {
+        var sign = 1;
+        if (number.StartsWith('-'))
+        {
+            number = number.Substring(1);
+            sign = -1;
+        }
+        if (number.StartsWith("0b", StringComparison.CurrentCultureIgnoreCase))
+        {
+            number = number.Substring(2);
+            var parsedBigInt = BigInteger.Zero;
+            var postionValue = BigInteger.One;
+            foreach (var ch in number.Reverse())
+            {
+                if (ch == '1')
+                {
+                    parsedBigInt += postionValue;
+                }
+                else if (ch != '0')
+                {
+                    value = 0.0;
+                    return false;
+                }
+                postionValue <<= 1;
+            }
+            if (parsedBigInt > DoubleMaxValueAsBigInt)
+            {
+                value = double.PositiveInfinity * sign;     
+            }
+            else
+            {
+                value = ((double)parsedBigInt) * sign;
+            }
+            return true;
+        }
+
+        value = 0.0;
+        return false;
+    }
+
+    static bool TryParseOctal(string number, out double value)
+    {
+        var sign = 1;
+        if (number.StartsWith('-'))
+        {
+            number = number.Substring(1);
+            sign = -1;
+        }
+        if (number.StartsWith("0o", StringComparison.CurrentCultureIgnoreCase))
+        {
+            number = number.Substring(2);
+            var parsedBigInt = BigInteger.Zero;
+            var postionValue = BigInteger.One;
+            foreach (var ch in number.Reverse())
+            {
+                if (ch >= '0' && ch <= '7')
+                {
+                    parsedBigInt += postionValue * (ch - '0');
+                }
+                else
+                {
+                    value = 0.0;
+                    return false;
+                }
+                postionValue <<= 3;
+            }
+            if (parsedBigInt > DoubleMaxValueAsBigInt)
+            {
+                value = double.PositiveInfinity * sign;     
+            }
+            else
+            {
+                value = ((double)parsedBigInt) * sign;
+            }
+            return true;
+        }
+
+        value = 0.0;
+        return false;
+    }
+}
