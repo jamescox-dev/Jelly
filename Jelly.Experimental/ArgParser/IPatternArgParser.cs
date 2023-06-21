68c89ebc2f318579405b06a544eefb1d54558371
James Cox <james@jamescox.info>
2023-06-21 20:22:20 +0100

    Quick spike to try out some ideas for a generic pattern based arg parser.

--- Jelly.Experimental/ArgParser/IPatternArgParser.cs
+++ Jelly.Experimental/ArgParser/IPatternArgParser.cs
@@ -5,17 +5,66 @@ using Jelly.Commands.ArgParsers;
 
 public interface IPatternArgParser
 {
-    int AcceptsMinArgs { get; }
-    int AcceptsMaxArgs { get; }
-
-    ParseResult Parse(string commandName, IReadOnlyList<Value> args, ImmutableSortedDictionary<Value, Value>.Builder result);
+    ParseResult Parse(IEnumerable<Value> args);
 }
 
+public record ParseResult(bool IsSuccessful) {}
 
-public record ParseResult(bool Successful) {}
+public record ParseSuccess(int ConsumedArgs, Dictionary<string, Value> ArgValues) : ParseResult(true) {}
 
-public record ParseSuccess(int ConsumedArguments, Dictionary<string, Value> argValues) : ParseResult(true) {}
-
-public record ParseMissingArg(Arg ExpectedArg) : ParseResult(false) {}
+public record ParseMissingArg(string ExpectedArgName) : ParseResult(false) {}
 
 public record ParseUnexpectedArg() : ParseResult(false) {}
+
+public class ArgParser : IPatternArgParser
+{
+    readonly string _name;
+
+    public ArgParser(string name)
+    {
+        _name = name;
+    }
+
+    public ParseResult Parse(IEnumerable<Value> args)
+    {
+        var arg = args.FirstOrDefault();
+        if (arg is not null)
+        {
+            return new ParseSuccess(1, new() { { _name, arg } });
+        }
+        return new ParseMissingArg(_name);
+    }
+}
+
+public class SeqArgParser : IPatternArgParser
+{
+    readonly IPatternArgParser[] _subParsers;
+
+    public SeqArgParser(params IPatternArgParser[] subParsers)
+    {
+        _subParsers = subParsers;
+    }
+
+    public ParseResult Parse(IEnumerable<Value> args)
+    {
+        var argEnum = args.GetEnumerator();
+        var argValues = new Dictionary<string, Value>();
+        foreach (var subParser in _subParsers)
+        {
+            var subResult = subParser.Parse(args.Skip(i));
+            if (subResult is ParseSuccess subSuccess)
+            {
+                i += subSuccess.ConsumedArgs;
+                foreach (var kvp in subSuccess.ArgValues)
+                {
+                    argValues.Add(kvp.Key, kvp.Value);
+                }
+            }
+            else
+            {
+                return subResult;
+            }
+        }
+        return new ParseSuccess(i, argValues);
+    }
+}
