7379347f73a33d9ecb099d2449accafdf117dc96
James Cox <james@jamescox.info>
2023-08-16 15:32:39 +0100

    Updated assignment and variable evaluators to use new indexer supporting scope get/set variable methods.

--- src/Jelly/Evaluator/AssignmentEvaluator.cs
+++ src/Jelly/Evaluator/AssignmentEvaluator.cs
@@ -5,70 +5,18 @@ internal class AssignmentEvaluator : IEvaluator
     public Value Evaluate(IEnv env, DictValue node)
     {
         var name = node.GetString(Keywords.Name);
-        var assignmentValue = env.Evaluate(node.GetNode(Keywords.Value));
-        var currentValue = assignmentValue;
-        if (node.ContainsKey(Keywords.Indexers))
+        var value = env.Evaluate(node.GetNode(Keywords.Value));
+
+        if (node.TryGetValue(Keywords.Indexers, out var indexers))
         {
-            currentValue = env.CurrentScope.GetVariable(name);
-            var previousValues = new Stack<Value>();
-            var indexers = node.GetList(Keywords.Indexers).Select(i => EvaluatedIndexer.EvaluateIndexerValue(env, i)).ToArray();
-
-            foreach (var indexer in indexers.Take(indexers.Length - 1))
-            {
-                previousValues.Push(currentValue);
-                currentValue = GetItem(currentValue, indexer);
-            }
-
-            currentValue = SetItem(currentValue, indexers.Last(), assignmentValue);
-
-            foreach (var indexer in indexers.Take(indexers.Length - 1).Reverse())
-            {
-                currentValue = SetItem(previousValues.Pop(), indexer, currentValue);
-            }
-        }
-        env.CurrentScope.SetVariable(name, currentValue);
-        return assignmentValue;
-    }
-
-    static Value GetItem(Value collection, EvaluatedIndexer indexer)
-    {
-        if (indexer.IsListIndexer)
-        {
-            var list = collection.ToListValue();
-            var index = indexer.Value.ToIndexOf(list);
-            return list[index];
+            var indexer = ValueIndexer.FromIndexerNodes(indexers.ToListValue(), env);
+            env.CurrentScope.SetVariable(name, indexer, value);
         }
         else
         {
-            var dict = collection.ToDictionaryValue();
-            var key = indexer.Value;
-            return dict[key];
+            env.CurrentScope.SetVariable(name, value);
         }
-    }
 
-    static Value SetItem(Value collection, EvaluatedIndexer indexer, Value value)
-    {
-        if (indexer.IsListIndexer)
-        {
-            var list = collection.ToListValue();
-            var index = indexer.Value.ToIndexOf(list);
-            return list.SetItem(index, value);
-        }
-        else
-        {
-            var dict = collection.ToDictionaryValue();
-            var key = indexer.Value;
-            return dict.SetItem(key, value);
-        }
-    }
-
-    record EvaluatedIndexer(bool IsListIndexer, Value Value)
-    {
-        public static EvaluatedIndexer EvaluateIndexerValue(IEnv env, Value indexer)
-        {
-            var indexerNode = indexer.ToNode();
-            return new(indexerNode[Keywords.Type] == Keywords.ListIndexer,
-                    env.Evaluate(indexerNode[Keywords.Expression].ToNode()));
-        }
+        return value;
     }
 }
--- src/Jelly/Evaluator/VariableEvaluator.cs
+++ src/Jelly/Evaluator/VariableEvaluator.cs
@@ -4,29 +4,14 @@ internal class VariableEvaluator : IEvaluator
 {
     public Value Evaluate(IEnv env, DictValue node)
     {
-        var value = env.CurrentScope.GetVariable(node.GetString(Keywords.Name));
+        var name = node.GetString(Keywords.Name);
 
-        if (node.TryGetValue(Keywords.Indexers, out var indexersValue))
+        if (node.TryGetValue(Keywords.Indexers, out var indexers))
         {
-            var indexers = indexersValue.ToListValue();
-            foreach (var indexerValue in indexers)
-            {
-                var indexer = indexerValue.ToNode();
-                if (indexer[Keywords.Type] == Keywords.ListIndexer)
-                {
-                    var list = value.ToListValue();
-                    var index = env.Evaluate(indexer[Keywords.Expression].ToNode()).ToIndexOf(list);
-                    value = list[index];
-                }
-                else
-                {
-                    var dict = value.ToDictionaryValue();
-                    var key = env.Evaluate(indexer[Keywords.Expression].ToNode());
-                    value = dict[key];
-                }
-            }
+            var indexer = ValueIndexer.FromIndexerNodes(indexers.ToListValue(), env);
+            return env.CurrentScope.GetVariable(name, indexer);
         }
 
-        return value;
+        return env.CurrentScope.GetVariable(name);
     }
 }
